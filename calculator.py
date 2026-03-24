87001c979193c13df18cc727c4d32f2ccc69cfe6
varshinipillai5@gmail.com <varshinipillai5@gmail.com>
2026-03-24 06:45:07 +0530

    added workflows

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""Simple calculator module providing basic arithmetic operations."""
+
+def add(number_a, number_b):
+    """Return the sum of two numbers."""
+    return number_a + number_b
+
+def subtract(number_a, number_b):
+    """Return the difference of two numbers."""
+    return number_a - number_b
